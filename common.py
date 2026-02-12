4d1efb04baf5faf001fa04223f05865418a62350
crivella <davidecrivella@yahoo.it>
2026-02-12 12:38:31 +0100

    Update defaults for faster demo

--- common.py
+++ common.py
@@ -34,18 +34,18 @@ STIFFNESS_DEFAULTS = {
 }
 
 VISCOSITY_WORKCHAIN_DEFAULTS = {
-    'num_steps': 5000,
+    'num_steps': 500,
     'smiles_string': 'CC(=C)C(=O)OC1C[C@H]2CC[C@]1(C)C2(C)C',
     'reference_temperature': 343,
     'deform_velocities': '0.005, 0.002, 0.05, 0.02, 0.01, 0.1, 0.2',
     'num_steps_minimization': 100,
-    'num_steps_equilibration': 5000,
+    'num_steps_equilibration': 500,
     'num_steps_min': 100,
-    'num_steps_eq': 5000,
+    'num_steps_eq': 500,
     'force_field': 'gaff',
     'nmols': 1000,
     'time_step': 0.001,
-    'averaging_start_time': 1,
+    'averaging_start_time': 0.1,
 }
 
 # Mapp function imput parameters to created code names
